a139de37f0fb1959122e934ddcf34148035798ea
Edwin Cardenas <ecardenas@lwolf.com>
2025-09-05 14:02:27 -0500

    Initial Commit

---
+++ FirebaseChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Edwin Cardenas on 9/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
