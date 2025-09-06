f7763e7923f7bc395e5148d51105557f324791ca
Edwin Cardenas <ecardenas@lwolf.com>
2025-09-06 17:55:14 -0500

    feat: set up login controller basic layout

--- FirebaseChat/Controller/Authentication/LoginController.swift
+++ FirebaseChat/Controller/Authentication/LoginController.swift
@@ -29,6 +29,39 @@ class LoginController: UIViewController {
         return imageView
     }()
 
+    let emailContainerView: UIView = {
+        let view = UIView()
+
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemCyan
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+
+        return view
+    }()
+
+    let passwordContainerView: UIView = {
+        let view = UIView()
+
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemYellow
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+
+        return view
+    }()
+
+    lazy var loginButton: UIButton = {
+        let button = UIButton(type: .system)
+
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Log in", for: .normal)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        button.backgroundColor = .systemRed
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+
+        return button
+    }()
+
     // MARK: - View Lifecycle
 
     override func viewDidLoad() {
@@ -58,15 +91,42 @@ extension LoginController {
     private func setupViews() {
         navigationController?.navigationBar.isHidden = true
 
+        let stackView = UIStackView(arrangedSubviews: [
+            emailContainerView,
+            passwordContainerView,
+            loginButton,
+        ])
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 16
+
         view.addSubview(iconImage)
+        view.addSubview(stackView)
 
         // iconImage
         NSLayoutConstraint.activate([
             iconImage.topAnchor.constraint(
                 equalTo: view.safeAreaLayoutGuide.topAnchor,
-                constant: 24
+                constant: 32
             ),
             iconImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+
+        // stackView
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(
+                equalTo: iconImage.bottomAnchor,
+                constant: 32
+            ),
+            stackView.leadingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                constant: 32
+            ),
+            stackView.trailingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+                constant: -32
+            ),
         ])
     }
 
